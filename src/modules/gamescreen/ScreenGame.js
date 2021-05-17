6ff3081168b4c89023577a370b00edabf8e95c0e
KaL844 <anthuynguyen2000@gmail.com>
2021-05-17 09:11:54 -0700

    Change design

--- src/modules/gamescreen/ScreenGame.js
+++ src/modules/gamescreen/ScreenGame.js
@@ -16,8 +16,8 @@ var ScreenGame = cc.Layer.extend({
 
         var size = cc.winSize;
         var margin = 10;
-        var marginTop = 30;
-        var marginBottom = 150;
+        var marginTop = 10;
+        var marginBottom = 0;
 
         // init vars
         this.state = new InitGameState();
@@ -30,7 +30,7 @@ var ScreenGame = cc.Layer.extend({
                 top_left: {
                     x: size.width-margin,
                     y: size.height-marginTop
-                }
+                },
             },
             grid: [40, 30]
         };
@@ -61,79 +61,77 @@ var ScreenGame = cc.Layer.extend({
         this._super();
         var size = cc.winSize;
 
-        // this.upArrow = new cc.Sprite('res/ar.png');
-        // this.upArrow.attr({
-        //     anchorX: 0.5,
-        //     anchorY: 0,
-        //     x: size.width / 2,
-        //     y: 90,
-        // })
-        // this.upArrow.setScale(0.125);
-        // this.addChild(this.upArrow);
-        //
-        // this.rightArrow = new cc.Sprite('res/ar.png');
-        // this.rightArrow.attr({
-        //     anchorX: 0.5,
-        //     anchorY: 0,
-        //     x: size.width / 2 + 40,
-        //     y: 75,
-        // })
-        // this.rightArrow.runAction(cc.RotateBy(0, 90))
-        // this.rightArrow.setScale(0.125);
-        // this.addChild(this.rightArrow);
-        //
-        // this.downArrow = new cc.Sprite('res/ar.png');
-        // this.downArrow.attr({
-        //     anchorX: 0.5,
-        //     anchorY: 0,
-        //     x: size.width / 2,
-        //     y: 60,
-        // })
-        // this.downArrow.runAction(cc.RotateBy(0, 180))
-        // this.downArrow.setScale(0.125);
-        // this.addChild(this.downArrow);
-        //
-        // this.rightArrow = new cc.Sprite('res/ar.png');
-        // this.rightArrow.attr({
-        //     anchorX: 0.5,
-        //     anchorY: 0,
-        //     x: size.width / 2 - 40,
-        //     y: 75,
-        // })
-        // this.rightArrow.runAction(cc.RotateBy(0, 270))
-        // this.rightArrow.setScale(0.125);
-        // this.addChild(this.rightArrow);
+        var grass = new cc.Sprite('res/grass.jpg');
+        grass.anchorX = 0;
+        grass.anchorY = 0;
+        this.addChild(grass);
 
+        var background = new cc.Sprite('res/background.jpg', cc.rect(0, 0, this.screen.width, this.screen.height));
+        background.anchorX = 0;
+        background.anchorY = 0;
+        background.setPosition(this.screen.border.bottom_right);
+        this.addChild(background);
 
-
-        // this.snake_pic = new cc.Sprite('res/green.jpg', cc.rect(0,0, this.screen.block.x, this.screen.block.y));
         this.snake_pic = new cc.Sprite('res/snakepic.png', cc.rect(64*3,0, 64, 64));
         this.snake_pic.attr({
             anchorX: 0.5,
-            anchorY: 0.5,
-            x: this.absolutePos(this.state.snake_dots[0]).x,
-            y: this.absolutePos(this.state.snake_dots[0]).y
+            anchorY:0.5,
         })
         this.snake_pic.setScale(0.5);
         this.addChild(this.snake_pic);
 
+        this.sntail = new cc.Sprite('res/snakepic.png', cc.rect(64*3,64*2, 64, 64));
+        this.sntail.attr({
+            anchorX: 0.5,
+            anchorY:0.5,
+        })
+        this.sntail.setScale(0.5);
+        this.sntail.setVisible(false);
+        this.addChild(this.sntail);
+
+        this.sntail1 = new cc.Sprite('res/snakepic.png', cc.rect(64*3,64*2, 64, 64));
+        this.sntail1.attr({
+            anchorX: 0.5,
+            anchorY:0.5,
+        })
+        this.sntail1.setScale(0.5);
+        this.sntail1.setVisible(false);
+        this.sntail1.runAction(cc.RotateBy(0, 90));
+        this.addChild(this.sntail1);
+
+        this.sntail2 = new cc.Sprite('res/snakepic.png', cc.rect(64*3,64*2, 64, 64));
+        this.sntail2.attr({
+            anchorX: 0.5,
+            anchorY:0.5,
+        })
+        this.sntail2.setScale(0.5);
+        this.sntail2.setVisible(false);
+        this.sntail2.runAction(cc.RotateBy(0, 180));
+        this.addChild(this.sntail2);
+
+        this.sntail3 = new cc.Sprite('res/snakepic.png', cc.rect(64*3,64*2, 64, 64));
+        this.sntail3.attr({
+            anchorX: 0.5,
+            anchorY:0.5,
+        })
+        this.sntail3.setScale(0.5);
+        this.sntail3.setVisible(false);
+        this.sntail3.runAction(cc.RotateBy(0, 270));
+        this.addChild(this.sntail3);
+
+
         // show score
         this.score = new cc.LabelTTF('', 'Arial', 20);
         this.score.x = size.width / 2;
-        this.score.y = size.height;
-        this.score.anchorY = 1;
+        this.score.y = size.height - 25;
+        this.score.anchorY = 0.5;
         this.addChild(this.score);
 
         // Game over
-        this.gameOverText = gv.customText("GAME OVER!!", size.width/2, 3*size.height/5, 60);
-        this.gameOverText.setVisible(false);
-        this.addChild(this.gameOverText);
+        // this.gameOverText = gv.customText("GAME OVER!!", size.width/2, 3*size.height/5, 60);
+        // this.gameOverText.setVisible(false);
+        // this.addChild(this.gameOverText);
 
-        // Return to menu
-        this.menuBtn = gv.commonButton(200, 64, size.width/2, 2.3*size.height/5, "Go to Menu");
-        this.addChild(this.menuBtn);
-        this.menuBtn.setVisible(false);
-        this.menuBtn.addClickEventListener(this.onMenuBtnClick.bind(this));
 
         this.drawBorder();
         this.updateDisplay();
@@ -213,6 +211,27 @@ var ScreenGame = cc.Layer.extend({
             }, this);
         }
 
+        this.gameOverText = new cc.Sprite('res/gameOver.png');
+        this.gameOverText.attr({
+            x: size.width/2,
+            y:  3*size.height/5,
+            anchorX: 0.5,
+            anchorY: 0.5
+        });
+        this.gameOverText.setScale(0.25);
+        this.gameOverText.setVisible(false);
+        this.addChild(this.gameOverText);
+
+        // Return to menu
+        // this.menuBtn = gv.commonButton(200, 64, size.width/2, 2.3*size.height/5, "Go to Menu");
+        this.menuBtn = new ccui.Button('res/menu.png', 'res/menuclicked.png', 'res/menu.png');
+        this.menuBtn.x = size.width/2;
+        this.menuBtn.y = 2.3*size.height/5;
+        this.menuBtn.setScale(0.5);
+        this.addChild(this.menuBtn);
+        this.menuBtn.setVisible(false);
+        this.menuBtn.addClickEventListener(this.onMenuBtnClick.bind(this));
+
         this.scheduleUpdate();
 
     },
@@ -241,12 +260,12 @@ var ScreenGame = cc.Layer.extend({
         }
 
         // score
-        this.score.setString('Score: ' + this.state.score);
+        this.score.setString('SCORE: ' + this.state.score);
 
 
         if (this.snake_body.length < this.state.len) {
             // var snake_tail = new cc.Sprite('res/green.jpg', cc.rect(0,0, this.screen.block.x, this.screen.block.y))
-            var snake_tail = new cc.Sprite('res/snakepic.png', cc.rect(64*2,64, 64, 64))
+            var snake_tail = new cc.Sprite('res/snakepic.png', cc.rect(64*2,64, 64, this.screen.block.y*2))
             snake_tail.attr({
                 anchorX: 0.5,
                 anchorY: 0.5,
@@ -259,7 +278,7 @@ var ScreenGame = cc.Layer.extend({
 
         if (this.snake_Hbody.length < this.state.len) {
             // var snake_htail = new cc.Sprite('res/green.jpg', cc.rect(0,0, this.screen.block.x, this.screen.block.y))
-            var snake_htail = new cc.Sprite('res/snakepic.png', cc.rect(64,0, 64, 64))
+            var snake_htail = new cc.Sprite('res/snakepic.png', cc.rect(64,0, this.screen.block.x*2, 64))
             snake_htail.attr({
                 anchorX: 0.5,
                 anchorY: 0.5,
@@ -318,10 +337,8 @@ var ScreenGame = cc.Layer.extend({
             this.snake_curve_4.push(snake_tail);
         }
 
-        for (var i = 0, len = this.state.snake_dots.length; i < len; ++i) {
-            // this.snake.drawDot(this.absolutePos(this.state.snake_dots[i]),
-            //     dotSize, cc.color(0,255,0));
-            if (i < this.state.snake_dots.length - 1) {
+        for (var i = 0, len = this.state.snake_dots.length - 1; i < len; ++i) {
+            if (i > 0) {
                 if (i > 0
                     && (this.state.snake_dots[i].x === this.state.snake_dots[i-1].x
                     && this.state.snake_dots[i].y === this.state.snake_dots[i+1].y
@@ -424,7 +441,41 @@ var ScreenGame = cc.Layer.extend({
                     this.snake_Hbody[i].setPosition(this.absolutePos(this.state.snake_dots[i]));
                 }
             }
+            else {
+                if (this.state.snake_dots[i].y < this.state.snake_dots[i+1].y) {
+                    this.sntail.setVisible(true);
+                    this.sntail1.setVisible(false);
+                    this.sntail2.setVisible(false);
+                    this.sntail3.setVisible(false);
+                    this.sntail.setPosition(this.absolutePos(this.state.snake_dots[i]))
+                }
+                else if (this.state.snake_dots[i].x < this.state.snake_dots[i+1].x) {
+                    this.sntail1.setVisible(true);
+                    this.sntail.setVisible(false);
+                    this.sntail2.setVisible(false);
+                    this.sntail3.setVisible(false);
+                    this.sntail1.setPosition(this.absolutePos(this.state.snake_dots[i]))
+                }
+                else if (this.state.snake_dots[i].x > this.state.snake_dots[i+1].x) {
+                    this.sntail3.setVisible(true);
+                    this.sntail.setVisible(false);
+                    this.sntail2.setVisible(false);
+                    this.sntail1.setVisible(false);
+                    this.sntail3.setPosition(this.absolutePos(this.state.snake_dots[i]))
+                }
+
+                else if (this.state.snake_dots[i].y > this.state.snake_dots[i+1].y) {
+                    this.sntail2.setVisible(true);
+                    this.sntail.setVisible(false);
+                    this.sntail1.setVisible(false);
+                    this.sntail3.setVisible(false);
+                    this.sntail2.setPosition(this.absolutePos(this.state.snake_dots[i]))
+                }
+            }
         }
+
+        this.snake_pic.setPosition(this.absolutePos(this.state.snake_dots[this.state.snake_dots.length - 1]));
+        this.sntail.setPosition(this.absolutePos(this.state.snake_dots[0]));
 
         this.apple = new cc.Sprite('res/snakepic.png', cc.rect(0,64*3,64,64));
         this.apple.attr({
@@ -455,19 +506,15 @@ var ScreenGame = cc.Layer.extend({
 
         switch (this.state.direction) {
             case 0:
-                this.snake_pic.runAction(cc.moveBy(0,cc.p(0,this.screen.block.y)));
                 head.y += 1;
                 break;
             case 1:
-                this.snake_pic.runAction(cc.moveBy(0,cc.p(this.screen.block.x,0)));
                 head.x += 1;
                 break;
             case 2:
-                this.snake_pic.runAction(cc.moveBy(0,cc.p(0,-this.screen.block.y)));
                 head.y -= 1;
                 break;
             case 3:
-                this.snake_pic.runAction(cc.moveBy(0,cc.p(-this.screen.block.x,0)));
                 head.x -= 1;
                 break;
         }
@@ -489,7 +536,7 @@ var ScreenGame = cc.Layer.extend({
         if (head.x === this.state.apple.x && head.y === this.state.apple.y) {
             this.state.score += 1;
             this.state.len += 1;
-            this.state.speed += 0.3;
+            this.state.speed += 0.1;
             this.generatePrey();
         }
 
--- src/modules/mainmenu/ScreenMenu.js
+++ src/modules/mainmenu/ScreenMenu.js
@@ -11,12 +11,26 @@ var ScreenMenu = cc.Layer.extend({
         this._super();
         var size = cc.director.getVisibleSize();
 
-        var btnStart = gv.commonButton(200, 64, cc.winSize.width/2, 1*size.height/2,"Game Start");
+        var background = new cc.Sprite('res/snake_pass.jpg');
+        background.anchorX = 0;
+        background.anchorY = 0;
+        this.addChild(background);
+
+        var btnStart = new ccui.Button('res/startbtn.png','res/startbtnclicked.png','res/startbtn.png');
+        btnStart.x = cc.winSize.width / 2;
+        btnStart.y = 0.75 * cc.winSize.height / 2;
+        btnStart.setScale(0.2);
         this.addChild(btnStart);
         btnStart.addClickEventListener(this.onSelectStart.bind(this));
 
-        var titleText = gv.customText("SNAKE GAME", cc.winSize.width/2, 3.5*size.height/5, 60);
-        this.addChild(titleText);
+        var title = new cc.Sprite('res/title.png');
+        title.x = cc.winSize.width / 2;
+        title.y = 3.5 * cc.winSize.height / 5;
+        title.setScale(1.5);
+        this.addChild(title);
+
+        // var titleText = gv.customText("SNAKE GAME", cc.winSize.width/2, 3.5*size.height/5, 60);
+        // this.addChild(titleText);
 
 
     },
